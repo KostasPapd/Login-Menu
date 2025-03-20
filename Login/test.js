8b4bd6aa945581e9f80782b58fdac510f17ee11e
Konstantinos Papadopoulos <144434036+KostasPapd@users.noreply.github.com>
2025-03-20 11:31:10 +0000

    fixed database connection

--- Login/test.js
+++ Login/test.js
@@ -1,16 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const supabaseUrl = process.env.DATABASE_URL
-const supabaseKey = process.env.DATABASE_PASS
+dotenv.config(); // Load environment variables
+
+const supabaseUrl = process.env.DATABASE_URL;
+const supabaseKey = process.env.DATABASE_PASS;
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-import { supabase } from './supabaseClient';
 
 async function fetchData() {
   const { data, error } = await supabase
-    .from('test') // Replace with your table name
+    .from('test')
     .select('*');
-
   if (error) {
     console.error('Error fetching data:', error);
   } else {
